3696ebcc8c35c1425d4822af08fee0f74eaba4bd
ganadabang <ganadabang@gmail.com>
2022-01-06 17:13:31 +0900

    Modify function parameters

--- util/mlx/mlx_img_init.c
+++ util/mlx/mlx_img_init.c
@@ -6,15 +6,20 @@
 /*   By: hyeonsok <hyeonsok@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/03 15:15:16 by hyeonsok          #+#    #+#             */
-/*   Updated: 2022/01/03 15:15:52 by hyeonsok         ###   ########.fr       */
+/*   Updated: 2022/01/06 17:07:42 by hyeonsok         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "mlxx.h"
 
-void	mlx_img_init(int x, int y, void *mlx, t_img *img)
+void	mlx_img_init(t_data *data)
 {
-	img->img = mlx_new_image(mlx, x, y);
-	img->addr = mlx_get_data_addr(\
-	img->img, &img->bits_per_pixel, &img->line_length, &img->endian);
+	t_win	*win;
+	t_img	*img;
+
+	win = &data->win;
+	img = &data->img;
+	img->image = mlx_new_image(data->mlx_ptr, win->width, win->height);
+	img->address = mlx_get_data_addr(\
+	img->ptr, &img->bpp, &img->width, &img->endian);
 }
--- util/mlx/mlx_img_pixel_put.c
+++ util/mlx/mlx_img_pixel_put.c
@@ -6,16 +6,16 @@
 /*   By: hyeonsok <hyeonsok@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/03 15:13:18 by hyeonsok          #+#    #+#             */
-/*   Updated: 2022/01/03 15:15:48 by hyeonsok         ###   ########.fr       */
+/*   Updated: 2022/01/06 17:03:40 by hyeonsok         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "mlxx.h"
 
-void	mlx_img_pixel_put(t_img *img, int x, int y, int color)
+void	mlx_img_pixel_put(t_img *img)
 {
 	char	*dest;
 
-	dest = img->addr + (y * img->line_length + x * (img->bits_per_pixel / 8));
-	*(unsigned int *)dst = color;
+	dest = img->addr + (y * img->width + x * (img->bpp / 8));
+	*(unsigned int *)dest = color;
 }
