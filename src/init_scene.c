699f17c27e0db410cf9e5af6100f7fe4d16ed564
Hyeonsong Kim <rlausthd@gmail.com>
2022-02-02 11:41:01 +0900

    Format; norminette 3.3.1

--- src/init_scene.c
+++ src/init_scene.c
@@ -12,33 +12,6 @@
 
 #include "minirt.h"
 
-static void	cleanup(t_data *data)
-{
-	t_obj	*curr;
-	void	*del;
-	int		i;
-
-	perror("malloc");
-	mlx_destroy_window(data->mlx_ptr, data->win.ptr);
-	mlx_destroy_image(data->mlx_ptr, data->img.ptr);
-	curr = data->first_obj;
-	while (curr)
-	{
-		free(curr->info);
-		del = curr;
-		curr = curr->next;
-		free(del);
-	}
-	if (data->scene.view.matrix)
-	{
-		i = -1;
-		while (++i < data->win.height)
-			free(data->scene.view.matrix[i]);
-	}
-	free(data->scene.view.matrix);
-	exit(1);
-}
-
 static void	init_cam(t_cam *cam)
 {
 	cam->focal_len = 1.f;
@@ -57,7 +30,7 @@ static int	alloc_view(t_data *data)
 	data->scene.view.matrix = (t_vec **)malloc(data->win.height \
 		* sizeof(t_vec));
 	if (!data->scene.view.matrix)
-		cleanup(data);
+		return (0);
 	memset(data->scene.view.matrix, 0, data->win.height * sizeof(t_vec));
 	i = -1;
 	while (++i < data->win.height)
@@ -65,9 +38,29 @@ static int	alloc_view(t_data *data)
 		data->scene.view.matrix[i] = (t_vec *)malloc(data->win.width \
 			* sizeof(t_vec));
 		if (!data->scene.view.matrix[i])
-			cleanup(data);
+			return (0);
 	}
-	return (0);
+	return (1);
+}
+
+static void	calculate_view_matrix(t_scene *scene, t_vec *left_top, \
+									double width, double height)
+{
+	int	i;
+	int	j;
+
+	j = -1;
+	while (++j < height)
+	{
+		i = -1;
+		while (++i < width)
+		{
+			scene->view.matrix[j][i] = vec_minus(vec_plus(vec_plus(*left_top, \
+				vec_mult(scene->cam.basis_i, i * scene->view.width / width)), \
+				vec_mult(scene->cam.basis_j, -j * scene->view.height / height)),
+					scene->cam.origin);
+		}
+	}
 }
 
 static void	init_view(t_scene *scene, double width, double height)
@@ -75,8 +68,6 @@ static void	init_view(t_scene *scene, double width, double height)
 	const double	pi = acos(-1);
 	t_vec			left_top;
 	t_vec			center;
-	int				i;
-	int				j;
 
 	scene->view.width = 2 * scene->cam.focal_len \
 		* tan(0.5 * pi * scene->cam.fov / 180);
@@ -86,22 +77,16 @@ static void	init_view(t_scene *scene, double width, double height)
 	left_top = vec_minus(center, \
 		vec_plus(vec_mult(scene->cam.basis_i, scene->view.width * 0.5), \
 		vec_mult(scene->cam.basis_j, scene->view.height * -0.5)));
-	j = -1;
-	while (++j < height)
-	{
-		i = -1;
-		while (++i < width)
-		{
-			scene->view.matrix[j][i] = vec_minus(vec_plus(vec_plus(left_top, \
-                vec_mult(scene->cam.basis_i, i * scene->view.width / width)), \
-                vec_mult(scene->cam.basis_j, -j * scene->view.height / height)), scene->cam.origin);
-		}
-	}
+	calculate_view_matrix(scene, &left_top, width, height);
 }
 
 void	init_scene(t_data *data)
 {
 	init_cam(&data->scene.cam);
-	alloc_view(data);
+	if (!alloc_view(data))
+	{
+		perror("alloc_view: malloc");
+		exit(1);
+	}
 	init_view(&data->scene, data->win.width, data->win.height);
 }
